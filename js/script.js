0bedc50b1282bd07c984f7ad78ec80417c6c9c7a
footGunDev <83507106+KNelTech@users.noreply.github.com>
2024-08-14 09:49:04 -0500

    vers + cleanup

--- js/script.js
+++ js/script.js
@@ -191,55 +191,6 @@ window.addEventListener("beforeunload", function () {
   stopWebcam();
 });
 
-// const video = document.getElementById("video");
-
-// function webcamAccess() {
-//   // Ensure the video element exists
-//   const video = document.getElementById("video");
-//   if (!video) {
-//     console.error("Video element not found.");
-//     return;
-//   }
-
-//   // Check if getUserMedia method is available
-//   if (
-//     navigator.mediaDevices &&
-//     typeof navigator.mediaDevices.getUserMedia === "function"
-//   ) {
-//     navigator.mediaDevices
-//       .getUserMedia({ video: true })
-//       .then(function (stream) {
-//         video.srcObject = stream;
-//         video.play().catch(function (playError) {
-//           // Handle potential errors that might occur when trying to play the video
-//           console.error("Error attempting to play video:", playError);
-//         });
-//         video.style.display = "block"; // Only display the video element if access is granted
-//         console.log("Webcam access granted.");
-//       })
-//       .catch(function (error) {
-//         showAlert({ message: "Error finding webcam. did you allow access?" });
-//         console.error("Error accessing webcam:", error);
-//       });
-//   } else {
-//     console.error("getUserMedia not supported by this browser.");
-//   }
-// }
-
-// const webcamTest = document.getElementById("webcamTest");
-// webcamTest.addEventListener("click", function () {
-//   webcamAccess();
-// });
-
-// // Cleanup webcam when done
-// window.addEventListener("beforeunload", function () {
-//   const stream = video.srcObject;
-//   if (stream) {
-//     const tracks = stream.getTracks();
-//     tracks.forEach((track) => track.stop());
-//   }
-// });
-
 /*
  **  Recording
  */
